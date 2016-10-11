6825c247dc4b4ec0946c1f56ac1d33d28912f753
Herman Dempere <nerucius@gmail.com>
2016-10-11 13:14:01 +0200

    Created Prefabs for Enemy, Home, Turret
    Also included base scripts for:
    - Enemy: Move to "home" tagged object
    - Tower: Scan all enemies and select closest as a target

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyBehavior : MonoBehaviour{
+
+    public float moveSpeed = 5f;
+
+    GameObject home;
+
+	// Use this for initialization
+	void Start () {
+        // Get the Scene's tower
+        home = GameObject.FindGameObjectWithTag("home");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (home == null)
+            return;
+
+        // Get direction vector to tower
+        Vector3 dir = home.transform.position - this.transform.position;
+        // Dont move on Y axis (Vertical)
+        dir.y = 0;
+
+        if (dir.sqrMagnitude < Mathf.Pow(3,2))
+            return;
+        // Move X units per second
+        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/TowerBehavior.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerBehavior : MonoBehaviour {
+    
+    public float range = 10f;
+
+    private float idleTime = 0f;
+    private float idleTimeout = 2f;
+    private GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if(target == null){
+
+            // Idle out if we just searched
+            idleTime -= Time.deltaTime;
+            if (idleTime > 0f)
+                return;
+
+            // Look for nearest GameObject with tag "enemy", within range
+            GameObject[] targetList = GameObject.FindGameObjectsWithTag("enemy");
+
+            foreach(GameObject t in targetList) {
+                // Calculate distance squared
+                float dist2 = (transform.position - t.transform.position).sqrMagnitude;
+                if(dist2 <= range * range) {
+                    target = t;
+                    break;
+                }
+            }
+
+            if (target != null) {
+                Debug.Log("Found enemy:" + target.ToString());
+            }else{
+                // Set an idle timeout to prevent performance impact of checking all enemies every frame
+                idleTime += idleTimeout;
+            }
+
+            return;
+        }
+
+        // TODO shoot code here
+
+
+	}
+}
