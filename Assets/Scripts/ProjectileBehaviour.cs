b3b17c7e8b8b25cb2e16437ec3dde2c6feb95da3
sdiaz7 <k0n0k.wiki@gmail.com>
2016-10-14 00:35:36 +0200

    New Projectile prefab, modified Enemy prefab
    Projectiles ready to be shot. They follow a free, straight uniform
    movement towards the target position.
    Enemy prefab self-destroys when colliding with its target (home), and
    takes damage from any object tagged as a projectile.

---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+    private Vector3 startPosition;   // Projectile's starting position;
+
+    // STATS
+    public float damage = 1f;
+    public float speed = 100.0f;
+
+    public Transform target;         // Target position.
+    public string targetTagName;     // Target tag.
+    public string parentTagName;     // Shooting entity's tag.
+    public float reach = 0.0f;       // Projectile is destroyed after exceeding its reach. Using reach=0 disables such feature.
+
+    // Collision management.
+    void OnTriggerEnter(Collider other)
+    {
+        // Projectile self destructs when colliding with any entity other than that which shot it.
+        if (other.gameObject.tag != parentTagName)
+        {
+            SelfDestroy();
+        }
+    }
+
+    //First called function. Main assignations
+    void Awake()
+    {
+        //Initializing the enemy position
+        startPosition = transform.position;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (target == null)
+        {
+            // Check if we have a target.
+            Debug.LogWarning("Projectile has not a target.");
+        }
+        else
+        {
+            transform.LookAt(target);
+            Update();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // The projectile keeps moving even if its target has been already destroyed.
+        TransformProjectile();
+    }
+
+    private bool ReachExceeded()
+    {
+        float traversedDist = Vector3.Distance(startPosition, transform.position);
+        return traversedDist >= reach;
+    }
+
+    private void TransformProjectile()
+    {
+        /*
+         * The projectile describes a linear trajectory towards its target. On subsequent sprints, it could be
+         * interesting to implement some parabolic movement.
+         */
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    protected virtual void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
