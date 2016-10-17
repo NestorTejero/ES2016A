85ab8cc28e7dced5f7343dc54d53ed03d1fd751e
Edgar Llorente <edgar@edgarllorente.com>
2016-10-17 17:00:47 +0200

    Cambios HUD torres + pruebas iniciales + bg transparencia

--- Assets/Scripts/GUIScripts/TowerButton.cs
+++ Assets/Scripts/GUIScripts/TowerButton.cs
@@ -4,12 +4,21 @@ using System.Collections;
 
 [Serializable]
 public class TowerButton {
+	public Texture Background;
 	public Texture Image;
+	public Texture ImageLocked;
+	public Boolean Locked;
 
 	public TowerButton () {}
 
 	public void draw(ScaledRect Position) {
-		GUI.DrawTexture(Position.getRect(), this.Image);
+		GUI.DrawTexture(Position.getRect(), this.Background);
+		if (this.Locked) {
+			GUI.DrawTexture(Position.getRect(), this.ImageLocked);
+		} else {
+			GUI.DrawTexture(Position.getRect(), this.Image);
+		}
+
 	}
 }
 
