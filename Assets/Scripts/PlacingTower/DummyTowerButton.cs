5dece54c2fdaf1065e2a8f214a8ab0ca6678e6c2
sdiaz7 <k0n0k.wiki@gmail.com>
2016-10-20 18:46:20 +0200

    Player can now place towers in the game map
    Tower placement still uses an auxiliary button, ouside of the regular
    GUI

--- Assets/Scripts/PlacingTower/DummyTowerButton.cs
+++ Assets/Scripts/PlacingTower/DummyTowerButton.cs
@@ -6,22 +6,24 @@ public class DummyTowerButton : MonoBehaviour {
     public GameObject[] towers; // list of different types of towers (we have only one)
     private TowerPlacement placement;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         placement = GetComponent<TowerPlacement>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     // Creating the button and giving it functionality
     void OnGUI()
     {
         // Calling method from TowerPlacement, where we give the functionality
-        if (GUI.Button(new Rect(Screen.width/10, Screen.height/10 , 100, 30), towers[0].name))
-        { 
+        if (GUI.Button(new Rect(Screen.width / 10, Screen.height / 10, 100, 30), towers[0].name))
+        {
             placement.SetItem(towers[0]);
         }
     }
