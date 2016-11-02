12824857e114d8fae1bf5e706cd7225a2a141102
kazusaki1 <marcgonzalezfernandez@gmail.com>
2016-11-02 20:17:06 +0100

    colours tower placing + differents enemies for wave

--- Assets/Scripts/PlacingTower/TowerPosition.cs
+++ Assets/Scripts/PlacingTower/TowerPosition.cs
@@ -11,19 +11,33 @@ public class TowerPosition : MonoBehaviour
     // (now only with towers)
     void OnTriggerEnter(Collider c)
     {
-        if (c.tag == "tower")
+        if (c.tag == "tower" || c.tag == "home" || c.tag == "enemy")
         {
             colliders.Add(c);
         }
+    }
+
+
+    void OnTriggerStay(Collider c)
+    {
+
+        if (c.tag == "tower" || c.tag == "home" || c.tag == "enemy")
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(255, 0, 0, 125);
+         
+
+        }
+
     }
 
     // If tower exits collision with an object remove it from the colliders list
     // (now only with towers)
     void OnTriggerExit(Collider c)
     {
-        if (c.tag == "tower")
+        if (c.tag == "tower" || c.tag == "home" || c.tag == "enemy")
         {
             colliders.Remove(c);
+            gameObject.GetComponent<Renderer>().material.color = new Color32(0, 255, 0, 125);
         }
     }
 }
