d2874b24adb145df427f35e2f3a5c27868ebb81f
Alex Rodríguez Yarza <ary@alexroyar.es>
2016-11-22 19:47:16 +0100

    Primera versión de mostrado de imagen al aumentar coins

--- Assets/Scripts/GameGUI/Elements/InterfaceMoney.cs
+++ Assets/Scripts/GameGUI/Elements/InterfaceMoney.cs
@@ -19,12 +19,56 @@ public class InterfaceMoney {
 
 	public InterfaceMoney () {}
 
-	public void Draw() {
+    #region Variables actualización money
+
+    public InterfaceContainer MoneyUpContainer = new InterfaceContainer();
+
+    
+    //public Texture TexturaMoneyUp;
+    //public ScaledRect PosiTionMoneyUp;
+    //public int MoneyUpTime;
+
+    private float MoneyUpDeltaTime = 5.0f;
+    private float LastDeltaTime = 0f;
+    private int LastMoney = -1;
+    private bool ShowCoinUp = false;
+    #endregion
+
+    public void Draw() {
 		this.Contenedor.Draw ();
 
-		this.Label.SetText (LogicConnector.getCredit ().ToString ());
+		this.Label.SetText (LogicConnector.getCredit().ToString ());
 		this.Label.Draw ();
-	}
+
+
+        var credit_now = LogicConnector.getCredit();
+        if (LastMoney == -1)
+            LastMoney = credit_now;
+
+        Debug.Log(String.Format("LastMoney: {0}, CreditNow: {1}, LastDeltaTime: {2}, TimeNow: {3}", LastMoney, credit_now, LastDeltaTime, Time.realtimeSinceStartup));
+
+
+        if (LastMoney > credit_now)
+        {
+            if (!ShowCoinUp)
+            {
+                ShowCoinUp = true;
+                LastDeltaTime = Time.realtimeSinceStartup;
+            }else
+            {
+                if(LastDeltaTime + MoneyUpDeltaTime < Time.realtimeSinceStartup)
+                {
+                    ShowCoinUp = false;
+                    LastMoney = credit_now;
+                }
+            }
+
+            if (ShowCoinUp)
+            {
+                MoneyUpContainer.Draw();
+            }
+        }
+    }
 }
 
 
