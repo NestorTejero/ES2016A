4cde1b28614ba8483dd94ddcdb1beca17b1f7128
sdiaz7 <k0n0k.wiki@gmail.com>
2016-11-26 15:47:37 +0100

    Corregido movimiento de la camara
    La camara solo puede moverse por un espacio limitado. Velocidad de
    scroll mas baja. Añadida funcionalidad nueva: centrar camara en un
    objetivo.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
 
-	public float moveSpeed = 250f;      // movement through keyboard arrows
+    public float moveSpeed = 250f;      // movement through keyboard arrows
     public float scrollSpeed = 250f;    // movement through mouse wheel
+
+    // Camera boundaries. User cannot place camera outside the space defined by this coordinates
+    public Vector3 minimumBoundary = new Vector3(10f, 20f, 10f);
+    public Vector3 maximumBoundary = new Vector3(190f, 80f, 120f);
+    // X,Z boundaries should be related to the size of the terrain. Altitude optimal boundary (Y) has to be tested.
+
+    public string targetTag = "home";
+
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag(targetTag);
+        if (go != null)
+            centerCamera(go);
+    }
 
     // Called once per frame
     void Update()
@@ -32,14 +47,47 @@ public class CameraMovement : MonoBehaviour {
         if (scroll != 0)
         {
             // Inversed Y axis
-            MoveCamera(new Vector3(0, -scroll * scrollSpeed * 100 * Time.deltaTime, 0));
+            MoveCamera(new Vector3(0, -scroll * scrollSpeed * Time.deltaTime, 0));
         }
     }
 
     // Manages camera movement on the XYZ world axis.
-    private void MoveCamera(Vector3 position)
+    private void MoveCamera(Vector3 movement)
     {
+        Vector3 position = gameObject.transform.position; // current position
+        Vector3 targetPosition = position + movement;     // position after movement
+
+        // Place target postion inside boundaries.
+        targetPosition = new Vector3(Mathf.Min(maximumBoundary.x, targetPosition.x),
+            Mathf.Min(maximumBoundary.y, targetPosition.y), Mathf.Min(maximumBoundary.z, targetPosition.z));
+        targetPosition = new Vector3(Mathf.Max(minimumBoundary.x, targetPosition.x),
+            Mathf.Max(minimumBoundary.y, targetPosition.y), Mathf.Max(minimumBoundary.z, targetPosition.z));
+
         // Using world coordinates allows the camera to maintain its original orientation.
-        transform.Translate(position, Space.World);
+        transform.Translate(targetPosition - position, Space.World);
+    }
+
+    // Places camera so it points to given game object.
+    public void centerCamera(GameObject go)
+    {
+        centerCamera(go.transform.position);
+    }
+
+    // Places camera so it points to given position.
+    public void centerCamera(Vector3 target)
+    {
+        // Camera rotation in the X-axis.
+        float angle = transform.rotation.eulerAngles.x;
+        // Camera height respecting imposed boundaries.
+        float height = Mathf.Max(minimumBoundary.y, Mathf.Min(maximumBoundary.y, transform.position.y));
+
+        /*
+         * Center camera:
+         *      - set camera in the same X position as target
+         *      - maintain the same y posityion
+         *      - set z position as target's z minus height * tan(angle)
+         * */
+        transform.position = new Vector3(target.x, height,
+            target.z - height * Mathf.Tan(angle * Mathf.Deg2Rad));
     }
 }
