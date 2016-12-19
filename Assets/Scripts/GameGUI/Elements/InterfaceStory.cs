adf042bc0da0a353009f92a67fd9ea37fc7dabe5
Edgar Llorente <edgar@edgarllorente.com>
2016-12-19 21:15:57 +0100

    Implemented story

---
+++ Assets/Scripts/GameGUI/Elements/InterfaceStory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public class InterfaceStory { 
+	private int LastRound = -1;
+	private float Delta = 0;
+	private float ShowTime = 10;
+	public ScaledRect position;
+	public InterfaceLabel box = new InterfaceLabel();
+	public String[] StoryBits;
+
+	public InterfaceStory () {}
+
+	public void Draw () {
+		if (LastRound < LogicConnector.getRound ()) {
+			Delta = Time.time; 
+			LastRound = LogicConnector.getRound ();
+		}
+
+		if (Delta + ShowTime > Time.time) {
+			GUI.Window (1111, position.getRect(), WindowFunc, "Historia");
+		}
+	}
+
+	public void WindowFunc(int id) {
+		if (StoryBits.Length > LogicConnector.getRound ()) {
+			box.SetText (StoryBits [LogicConnector.getRound ()]);
+			box.Draw ();
+		}
+
+	}
+}
+
